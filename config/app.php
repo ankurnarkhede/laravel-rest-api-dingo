c7de43d7b00e72544e85a7ba3a759b979593fc71
smartankur4u <ankurnarkhede@gmail.com>
2018-02-27 17:38:32 +0530

    gitignore

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'fCCVkZNnKQerl8pKrgFwa1rAHmU05S2N'),
 
     'cipher' => 'AES-256-CBC',
 
